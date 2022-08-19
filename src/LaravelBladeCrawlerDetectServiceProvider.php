1f74bfacdba480f8ba56ba1b1ff95ea4fae997b0
Vladislav Stoitsov <dev@vladko.dev>
2022-08-19 22:01:29 +0300

    add lighthouse to crawlers

--- src/LaravelBladeCrawlerDetectServiceProvider.php
+++ src/LaravelBladeCrawlerDetectServiceProvider.php
@@ -4,6 +4,9 @@ namespace Vlados\LaravelBladeCrawlerDetect;
 
 use Illuminate\Support\Facades\Blade;
 use Jaybizzle\CrawlerDetect\CrawlerDetect;
+use Jaybizzle\CrawlerDetect\Fixtures\Crawlers;
+use Jaybizzle\CrawlerDetect\Fixtures\Exclusions;
+use Jaybizzle\CrawlerDetect\Fixtures\Headers;
 use Jaybizzle\LaravelCrawlerDetect\Facades\LaravelCrawlerDetect;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -19,8 +22,26 @@ class LaravelBladeCrawlerDetectServiceProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
-        Blade::if('user', function () {
-             return !app(CrawlerDetect::class)->isCrawler();
+        $crawlerDetect = app(CrawlerDetect::class);
+
+        $crawlers = new Crawlers();
+        $exclusions = new Exclusions();
+
+        $crawlerList = $crawlers->getAll();
+        $crawlerList[] = 'Chrome-Lighthouse';
+        $crawlerList[] = 'Google Page Speed';
+        $compiledRegex = $crawlerDetect->compileRegex($crawlerList);
+        $compiledExclusions = $crawlerDetect->compileRegex($exclusions->getAll());
+
+        $agent = trim(preg_replace(
+            "/{$compiledExclusions}/i",
+            '',
+            invade($crawlerDetect)->userAgent ?: ''
+        ));
+
+
+        Blade::if('user', function () use ($agent, $compiledRegex) {
+            return (bool) !preg_match("/{$compiledRegex}/i", $agent);
         });
     }
 
